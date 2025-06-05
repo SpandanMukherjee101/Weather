120287ad83f2f5b3e1d59cc7a8070668ed243de0
Spandan Mukherjee <tandram2013@gmail.com>
2025-06-05 20:44:57 +0530

    AWS bug fix

--- backend_microservices/api-gateway/index.js
+++ backend_microservices/api-gateway/index.js
@@ -6,12 +6,11 @@ const proxy = require('express-http-proxy');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: 'https://weather-sm.vercel.app', credentials: true }));
 app.use(express.json());
 
 app.use("/auth", proxy("http://auth-service:3001"));
 app.use("/user", proxy("http://user-service:3002"));
 app.use("/weather", proxy("http://weather-service:3003"));
 
-app.listen(3000, () => console.log('API Gateway running on port 3000'));
-//{ origin: 'https://weather-sm.vercel.app', credentials: true }
+app.listen(3000, '0.0.0.0', () => console.log('API Gateway running on port 3000'));
