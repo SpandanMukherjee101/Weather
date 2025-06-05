275d2b984ce8e2c7c6ddaa82011c9f4fa6137720
Spandan Mukherjee <tandram2013@gmail.com>
2025-06-05 18:30:30 +0530

    bug fix

--- backend_microservices/api-gateway/index.js
+++ backend_microservices/api-gateway/index.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", proxy("http://localhost:3001"));
-app.use("/user", proxy("http://localhost:3002"));
-app.use("/weather", proxy("http://localhost:3003"));
+app.use("/auth", proxy("http://auth-service:3001"));
+app.use("/user", proxy("http://user-service:3002"));
+app.use("/weather", proxy("http://weather-service:3003"));
 
 app.listen(3000, () => console.log('API Gateway running on port 3000'));
 //{ origin: 'https://weather-sm.vercel.app', credentials: true }
