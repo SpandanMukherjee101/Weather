196ff862c94511f1bf5091cfb5d07abe4fab53c1
Spandan Mukherjee <tandram2013@gmail.com>
2026-01-10 10:35:57 +0530

    removed microservices

--- backend_microservices/weather-service/routes/weather.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getWeather, updateCity, deleteCity } = require('../controllers/weatherController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-const router = express.Router();
-router.post('/', authMiddleware, getWeather);
-router.put('/update-city', authMiddleware, updateCity);
-router.delete('/delete-city', authMiddleware, deleteCity);
-module.exports = router;
